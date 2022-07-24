f670b8def22ab0092f6ac894194bfcbf2d792d75
Djek LU(kraken) <djeklu@gmail.com>
2022-07-24 11:30:17 +0300

    [+] все наработки по API на  24.07.2022

---
+++ app/Http/Resources/StorageAllowedGoodsResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StorageAllowedGoodsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'goods_id' => $this->goods_id,
+            'name' => $this->goods->name,
+            'unit' => $this->goods->unit,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::get('/get', \App\Http\Controllers\GetController::class  );
+    Route::get('/getMyStorage', [\App\Http\Controllers\ApiController::class, 'getMyStorage']);
+    Route::get('/getStorageProp/{id}', [\App\Http\Controllers\ApiController::class, 'getStorageProp']);
+    Route::get('/getStorageGoodsAvailable/{id}', [\App\Http\Controllers\ApiController::class, 'getStorageGoodsAvailable']);
+    Route::get('/getStorageOrderIn/{id}', [\App\Http\Controllers\ApiController::class, 'getStorageOrderIn']);
+
+    Route::post('/goodsMovementPush', [\App\Http\Controllers\ApiController::class, 'goodsMovementPush']);
+    Route::post('/goodsMovementPull', [\App\Http\Controllers\ApiController::class, 'goodsMovementPull']);
+    Route::post('/setPrice', [\App\Http\Controllers\ApiController::class, 'setPrice']);
+    Route::get('/getStorageGoodsAllowed/{id}', [\App\Http\Controllers\ApiController::class, 'getStorageGoodsAllowed']);
+    Route::post('/createOrder', [\App\Http\Controllers\ApiController::class, 'createOrder']);
+    Route::get('/getMainStorage', [\App\Http\Controllers\ApiController::class, 'getMainStorage']);
 });
 
 
