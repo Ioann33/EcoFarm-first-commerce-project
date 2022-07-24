775ab27eaa1915c1561f8d6adac4a52d082f66a5
Djek LU(kraken) <djeklu@gmail.com>
2022-07-24 11:21:44 +0300

    запусил санктум поставил телеском только для дев инсталировал auth контролллер и веб проверил работу /api/get через полную авторизацю+санктум

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function (){
-    return view('app');
-})->where('any', '.*');
 
 
 //Route::get('/', function () {
 //    return view('app');
 //});
+
+Auth::routes();
+
+Route::get('/{any}', function (){
+    return view('app');
+})->where('any', '.*');
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
