691e15b65cba3cb503597f20c3c7a822f80cb931
ioann <ivan.karmalyk2017@gmail.com>
2022-08-04 20:46:26 +0300

    optimization code

--- app/Http/Controllers/Reports/ReportController.php
+++ app/Http/Controllers/Reports/ReportController.php
@@ -11,15 +11,30 @@ use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
+
+    public $moneyModel;
+
+    public $movementsModel;
+
+
+    public function __construct()
+    {
+        $this->moneyModel = Money::all();
+        $this->movementsModel = Movements::all();
+    }
+
     public function getListGoodsMovements(Request $request){
 
-        $listGoodsMovement = Movements::all()->where('storage_id_from', '=', $request->storage_id)->where('date_accepted','>=', $request->date_from)->where('date_accepted','<=', $request->date_to);
+        $listGoodsMovement = $this->movementsModel
+            ->where('storage_id_from', '=', $request->storage_id)
+            ->where('date_accepted','>=', $request->date_from)
+            ->where('date_accepted','<=', $request->date_to);
         return ListGoodsMovementResource::collection($listGoodsMovement);
 
     }
 
     public function getSumMoneyMovementGoods(Request $request){
-        $listGoodsMovement = Movements::all()
+        $listGoodsMovement = $this->movementsModel
             ->where('storage_id_from', '=', $request->storage_id)
             ->where('date_accepted','>=', $request->date_from)
             ->where('date_accepted','<=', $request->date_to)
@@ -30,7 +45,11 @@ class ReportController extends Controller
         return ['sum'=>$listGoodsMovement];
     }
      public function getSalary(Request $request){
-         $listSalary = Money::all()->where('param_id', '=', $request->storage_id)->where('category', '=', $request->category_id)->where('date', '>=', $request->date_from)->where('date', '<=', $request->date_to);
+         $listSalary = $this->moneyModel
+             ->where('param_id', '=', $request->storage_id)
+             ->where('category', '=', $request->category_id)
+             ->where('date', '>=', $request->date_from)
+             ->where('date', '<=', $request->date_to);
 
         if ($request->type === 'list'){
             return ListSalaryResource::collection($listSalary);
@@ -42,10 +61,24 @@ class ReportController extends Controller
      }
 
      public function getSaldo(Request $request){
-        $salary = Money::all()->where('param_id', '=', $request->storage_id)->where('category', '=', 1)->where('date', '>=', $request->date_from)->where('date', '<=', $request->date_to)->sum('size_pay');
-        $spending = Money::all()->where('param_id', '=', $request->storage_id)->where('category', '=', 2)->where('date', '>=', $request->date_from)->where('date', '<=', $request->date_to)->sum('size_pay');
+        $salary = $this->moneyModel
+            ->where('param_id', '=', $request->storage_id)
+            ->where('category', '=', 1)->where('date', '>=', $request->date_from)
+            ->where('date', '<=', $request->date_to)
+            ->sum('size_pay');
+        $spending = $this->moneyModel
+            ->where('param_id', '=', $request->storage_id)
+            ->where('category', '=', 2)
+            ->where('date', '>=', $request->date_from)
+            ->where('date', '<=', $request->date_to)
+            ->sum('size_pay');
         $profit = $this->getSumMoneyMovementGoods($request);
-        $result =(int) $profit['sum'] - (int) $salary - (int) $spending;
-        return response()->json(['saldo' => $result]);
+        $saldo =(int) $profit['sum'] - (int) $salary - (int) $spending;
+        return response()->json([
+            'salary'=>$salary,
+            'spending'=>$spending,
+            'profit'=>$profit['sum'],
+            'saldo' => $saldo,
+            ]);
      }
 }
