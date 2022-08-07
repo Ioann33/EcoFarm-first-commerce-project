c469c0c08bd7e627dc194b4058a5eb7615045296
Djek LU(kraken) <djeklu@gmail.com>
2022-08-07 23:09:07 +0300

    1. произошло изменение название апи getMyStorage на getListMyStorages 2. добавил type в вывод этого апи

--- app/Http/Controllers/Finance/FinanceController.php
+++ app/Http/Controllers/Finance/FinanceController.php
@@ -7,7 +7,15 @@ use Illuminate\Http\Request;
 
 class FinanceController extends Controller
 {
-    function getFinance(Request $request){
+    function getFinance(Request $request): array
+    {
         return array('balance' => 432);
     }
+
+    function doSalary(Request $request): array
+    {
+        //dd($request);
+        return array('status' => 'sd');
+    }
+
 }
