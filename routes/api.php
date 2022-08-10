e3372eaf1537121a520f24ac1e75d3189c4632c2
Djek LU(kraken) <djeklu@gmail.com>
2022-08-10 12:17:41 +0300

    tune controller fix front в передаче товара, если это не теплица

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::get('/getMainStorage', [\App\Http\Controllers\Storage\StorageController::class, 'getMainStorage']);
     Route::get('/getListOrder/{dir}/{status}/{id}', [\App\Http\Controllers\Orders\OrderController::class, 'getListOrder']);
     Route::get('/setOrderStatus/{status}/{id}', [\App\Http\Controllers\Orders\OrderController::class, 'setOrderStatus']);
-    Route::get('/getStorageGoods/{key}/{id}/{goods_id?}', [\App\Http\Controllers\Goods\GoodsController::class, 'getStorageGoods']);
+    Route::get('/getStorageGoods/{key}/{storage_id}/{goods_id?}', [\App\Http\Controllers\Goods\GoodsController::class, 'getStorageGoods']);
     Route::get('/getMovement/{dir}/{status}/{id}', [\App\Http\Controllers\Goods\GoodsController::class, 'getMovement']);
     Route::get('/getOrder/{order_id}', [\App\Http\Controllers\Orders\OrderController::class, 'getOrder']);
 
