6610663404fb3a2a5ab20c1745838b05e4219af1
ioann <ivan.karmalyk2017@gmail.com>
2022-08-18 14:27:32 +0300

    improve getMovementInfo method, added field goods_type to response

--- app/Http/Controllers/Storage/StorageController.php
+++ app/Http/Controllers/Storage/StorageController.php
@@ -40,6 +40,20 @@ class StorageController extends Controller
         return StorageResource::collection($storage);
     }
 
+    public function updateStorage(Request $request, LogService $service){
+        $storage = Storages::findOrFail($request->storage_id);
+        $oldName = $storage->name;
+        $storage->name = $request->name;
+        $oldType = $storage->type;
+        $storage->type = $request->type;
+        if ($storage->save()){
+            $service->newLog('updateStorage', 'changed storage name from ('.$oldName.') to ('.$request->name.'), type from ('.$oldType.') to ('. $request->type, $request->storage_id);
+            return response()->json(['status'=>'ok', 'message'=>'changed storage name from ('.$oldName.') to ('.$request->name.'), type from ('.$oldType.') to ('. $request->type]);
+        }else{
+            return response()->json(['status'=>'error', 'message'=>'edit storage is failed']);
+        }
+    }
+
     public function addStorage(Request $request, LogService $service){
         $newStorage = new Storages();
         $newStorage->name = $request->name;
