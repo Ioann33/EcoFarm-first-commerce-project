86241ae04161103ee6e646ce154d6c48901b0377
ioann <ivan.karmalyk2017@gmail.com>
2022-08-18 15:51:39 +0300

    improve get movement info method added names by id

--- app/Http/Resources/GetMovementInfoResource.php
+++ app/Http/Resources/GetMovementInfoResource.php
@@ -14,15 +14,37 @@ class GetMovementInfoResource extends JsonResource
      */
     public function toArray($request)
     {
+        if ($this->storage_id_from != null){
+            $storageNameFrom = $this->storageFrom->name;
+        }else{
+            $storageNameFrom = null;
+        }
+
+        if ($this->storage_id_to != null){
+            $storageNameTo = $this->storageTo->name;
+        }else{
+            $storageNameTo = null;
+        }
+
+        if ($this->user_id_accepted != null){
+            $userAcceptedName = $this->userAccepted->name;
+        }else{
+            $userAcceptedName = null;
+        }
+
         return [
             "id"=>$this->id,
             "user_id_created" => $this->user_id_created,
+            "user_name_created" => $this->user->name,
             "date_created"=> $this->date_created,
             "storage_id_from" => $this->storage_id_from,
+            "storage_name_from" => $storageNameFrom,
             "storage_id_to" => $this->storage_id_to,
+            "storage_name_to" => $storageNameTo,
             "goods_id" => $this->goods_id,
             "amount" => $this->amount,
             "user_id_accepted" => $this->user_id_accepted,
+            "user_name_accepted" => $userAcceptedName,
             "price" => $this->price,
             "order_main" => $this->order_main,
             "date_accepted" => $this->date_accepted,
