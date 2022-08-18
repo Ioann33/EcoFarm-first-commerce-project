52ce91d91252fa9c85cb323dbebcebb47b7f2ff8
ioann <ivan.karmalyk2017@gmail.com>
2022-08-18 17:04:14 +0300

    small fix

--- app/Http/Resources/GetMovementInfoResource.php
+++ app/Http/Resources/GetMovementInfoResource.php
@@ -42,6 +42,7 @@ class GetMovementInfoResource extends JsonResource
             "storage_id_to" => $this->storage_id_to,
             "storage_name_to" => $storageNameTo,
             "goods_id" => $this->goods_id,
+            "goods_name" => $this->goods->name,
             "amount" => $this->amount,
             "user_id_accepted" => $this->user_id_accepted,
             "user_name_accepted" => $userAcceptedName,
