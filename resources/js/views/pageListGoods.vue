659c2d0f649faaf173971c9dfce291d163a9499b
Nazarii Solopchuk <nazarii.solopchuk@u10.studio>
2022-08-18 23:11:52 +0300

    tabs list goods page

--- resources/js/views/pageListGoods.vue
+++ resources/js/views/pageListGoods.vue
@@ -38,30 +38,85 @@
                     </div>
                     <div class="clearfix mb-3"></div>
                     <div data-bs-parent="#tab-group-2" class="collapse show" id="tab-5">
+                        <!--  Вид-1  -->
+                        <div class="content" id="tab-group-11">
 
-<!--  Вид-1  -->
-                        <div class=" mb-0">
-                            <table class="table text-center rounded-sm shadow-l" style="overflow: hidden;"> <!-- table-borderless -->
-                                <thead>
-                                <tr class="bg-grass-light">
-                                    <th scope="col" class="color-black opacity-50">Продукт</th>
-                                    <th scope="col" class="color-black opacity-50">Цена</th>
-                                    <th scope="col" class="color-black opacity-50">Количество</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr v-for="(goods, index) in listGoods" :key="goods.id">
-                                    <th scope="row" class="align-self-center ">{{ goods.name }}</th>
-                                    <td ><h4 class="font-600">{{ goods.price }} <sup class="font-400">₴</sup></h4></td>
-                                    <td><h4 class="font-600">{{ goods.amount }} <sup class="font-400">{{ goods.unit }}</sup></h4></td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="text-center mt-2" style="font-size: 18px;">
-                            ИТОГО: {{ Number.parseInt(totalAmount.split('.')[0]).toLocaleString('en-US').replace(',', ' ') }},{{ totalAmount.split('.')[1]}} грн
-                        </div>
+                            <div class="tab-controls tabs-small tabs-rounded mb-2" data-highlight="bg-blue-dark">
+                                <a href="#" data-bs-toggle="collapse" data-bs-target="#tab-15" class="bg-blue-dark no-click">Продукты</a>
+                                <a href="#" data-bs-toggle="collapse" data-bs-target="#tab-16" class="collapsed" @click.once="changeRule('ingredients')">Ингредиенты</a>
+                                <a href="#" data-bs-toggle="collapse" data-bs-target="#tab-17" class="collapsed">Все</a>
+                            </div>
 
+                            <div data-bs-parent="#tab-group-11" class="collapse show" id="tab-15" style="">
+                                <div class=" mb-0">
+                                    <table class="table text-center rounded-sm shadow-l" style="overflow: hidden;"> <!-- table-borderless -->
+                                        <thead>
+                                            <tr class="bg-grass-light">
+                                                <th scope="col" class="color-black opacity-50">Продукт</th>
+                                                <th scope="col" class="color-black opacity-50">Цена</th>
+                                                <th scope="col" class="color-black opacity-50">Количество</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr v-for="(goods, index) in listGoods.filter(el => el.type === 1 )" :key="goods.id">
+                                                    <th scope="row" class="align-self-center ">{{ goods.name }}</th>
+                                                    <td ><h4 class="font-600">{{ goods.price }} <sup class="font-400">₴</sup></h4></td>
+                                                    <td><h4 class="font-600">{{ goods.amount }} <sup class="font-400">{{ goods.unit }}</sup></h4></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="text-center mt-2" style="font-size: 18px;">
+                                    ИТОГО: {{ Number.parseInt(totalAmount1.split('.')[0]).toLocaleString('en-US').replace(',', ' ') }},{{ totalAmount1.split('.')[1]}} грн
+                                </div>
+                            </div>
+                            <div data-bs-parent="#tab-group-11" class="collapse" id="tab-16" style="">
+                                <div class=" mb-0">
+                                    <table class="table text-center rounded-sm shadow-l" style="overflow: hidden;"> <!-- table-borderless -->
+                                        <thead>
+                                        <tr class="bg-grass-light">
+                                            <th scope="col" class="color-black opacity-50">Продукт</th>
+                                            <th scope="col" class="color-black opacity-50">Цена</th>
+                                            <th scope="col" class="color-black opacity-50">Количество</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr v-for="(goods, index) in listGoods.filter(el => el.type === 2 )" :key="goods.id">
+                                            <th scope="row" class="align-self-center ">{{ goods.name }}</th>
+                                            <td ><h4 class="font-600">{{ goods.price }} <sup class="font-400">₴</sup></h4></td>
+                                            <td><h4 class="font-600">{{ goods.amount }} <sup class="font-400">{{ goods.unit }}</sup></h4></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="text-center mt-2" style="font-size: 18px;">
+                                    ИТОГО: {{ Number.parseInt(totalAmount2.split('.')[0]).toLocaleString('en-US').replace(',', ' ') }},{{ totalAmount2.split('.')[1]}} грн
+                                </div>
+                            </div>
+                            <div data-bs-parent="#tab-group-11" class="collapse" id="tab-17" style="">
+                                <div class=" mb-0">
+                                    <table class="table text-center rounded-sm shadow-l" style="overflow: hidden;"> <!-- table-borderless -->
+                                        <thead>
+                                        <tr class="bg-grass-light">
+                                            <th scope="col" class="color-black opacity-50">Продукт</th>
+                                            <th scope="col" class="color-black opacity-50">Цена</th>
+                                            <th scope="col" class="color-black opacity-50">Количество</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr v-for="(goods, index) in listGoods" :key="goods.id">
+                                            <th scope="row" class="align-self-center ">{{ goods.name }}</th>
+                                            <td ><h4 class="font-600">{{ goods.price }} <sup class="font-400">₴</sup></h4></td>
+                                            <td><h4 class="font-600">{{ goods.amount }} <sup class="font-400">{{ goods.unit }}</sup></h4></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="text-center mt-2" style="font-size: 18px;">
+                                    ИТОГО: {{ Number.parseInt(total.split('.')[0]).toLocaleString('en-US').replace(',', ' ') }},{{ total.split('.')[1]}} грн
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                     <div data-bs-parent="#tab-group-2" class="collapse" id="tab-6">
@@ -123,11 +178,27 @@ export default {
         }
     },
     computed: {
-      totalAmount() {
+      totalAmount1() {
           let total = 0;
-          this.listGoods.forEach(el => total += el.amount * el.price);
+          this.listGoods.forEach(el => {
+              if(el.type === 1) total += el.amount * el.price
+          });
           return total.toFixed(2);
-      }
+      },
+        totalAmount2() {
+            let total = 0;
+            this.listGoods.forEach(el => {
+                if(el.type === 2) total += el.amount * el.price
+            });
+            return total.toFixed(2);
+        },
+        total() {
+            let total = 0;
+            this.listGoods.forEach(el => {
+                total += el.amount * el.price
+            });
+            return total.toFixed(2);
+        }
     },
     updated() {
         update_template()
@@ -147,6 +218,9 @@ export default {
             this.message = 'Error: ('+err.response.status+'): '+err.response.data.message;
             console.log(this.message)
         })
+    },
+    methods: {
+
     }
 }
 </script>
