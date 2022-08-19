0c283d7d137322fe6c6f0873f9e306a251dba621
ioann <ivan.karmalyk2017@gmail.com>
2022-08-19 12:15:48 +0300

    changed method all() to get() in request to model

--- app/Http/Resources/StoragesPropResource.php
+++ app/Http/Resources/StoragesPropResource.php
@@ -14,6 +14,10 @@ class StoragesPropResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'type'=>$this->type,
+        ];
     }
 }
