ec258aafff03aa75d531fae33a78e2b1d015723c
Nazarii Solopchuk <nazarii.solopchuk@u10.studio>
2022-08-26 23:56:45 +0300

    component listMovements

--- resources/js/views/pageListMovements.vue
+++ resources/js/views/pageListMovements.vue
@@ -4,33 +4,7 @@
         <nav-bar></nav-bar>
 
         <div class="page-content header-clear-medium">
-            <div class="card card-style p-4 pt-0 mt-0" v-for="(item, index) in movements" :key="index">
-
-                <div class="row" >
-                    <div class="col-9">
-                        <div class="row mb-0">
-                            <div class="col-12 pt-2 pb-2" style="border-bottom: solid 1px #f2f2f7;">
-                                <span class="opacity-20">От </span>
-                                {{ item.storage_name_from }} <span class="opacity-30"> {{ item.user_name_created }}</span>
-
-<!--                                <span class="opacity-20">для </span>-->
-<!--                                {{ item.storage_name_to }} <span class="opacity-20">{{ item.user_name_accepted }}</span>-->
-                            </div>
-                            <div class="col-12 pt-2 pb-2" style="border-bottom: solid 1px #f2f2f7;">{{ item.goods_name }} {{ item.amount }} {{ item.goods_unit }}</div>
-                            <div class="col-12 pt-2 pb-2">
-                                ➠ <span class="opacity-20">для </span>
-                                {{ item.storage_name_to }} <span class="opacity-30">{{ item.user_name_accepted }}</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 d-flex align-items-center justify-content-center">
-                        <div class="icon icon-m rounded-md shadow-l me-1 bg-twitter"><i class="fa-solid fa-dolly"></i></div>
-                    </div>
-                    <div style="width: 100%; height: 2px; background-image: linear-gradient(to right, #A0D468, #8CC152)"></div>
-                    <div style="text-align: right;">{{ item.date_accepted }}</div>
-                </div>
-
-            </div>
+            <list-movements :storage_id="my_storage"></list-movements>
         </div>
 
         <nav-bar-menu></nav-bar-menu>
@@ -43,24 +17,26 @@
     import NavBarMenu from "../Components/NavBarMenu";
     import error from "../Components/Error";
     import TitlePage from "../Components/Title";
+    import listMovements from "../Components/listMovements";
 
     export default {
         name: "pagePreSale",
         components: {
             error,
             TitlePage,
-            headBar, NavBar, NavBarMenu,
+            headBar, NavBar, NavBarMenu, listMovements
         },
         data(){
             return {
-                movements: []
+                my_storage: ''
             }
         },
         computed: {},
         mounted() {
-            this.getListGoodsMovements();
+            this.my_storage = localStorage.getItem('my_storage_id');
         },
         updated() {
+
 
         },
         methods: {
