f7efc2e50f6a317f42c15a74deb6922ea2bca566
Nazarii Solopchuk <nazarii.solopchuk@u10.studio>
2022-08-26 23:05:41 +0300

    init listMovements component

---
+++ resources/js/Components/listMovements.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "listMovements",
+        props: {},
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
