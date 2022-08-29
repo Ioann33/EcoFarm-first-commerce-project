122c2dd4483bdc60417c6b153cf018e947167982
Djek LU(kraken) <djeklu@gmail.com>
2022-08-29 23:47:49 +0300

    1) модифицировал <list-movements>. теперь он кушает как goods_id так и storage_id 2) на главном складе/товары на складах: появилась вкладка - движение товара

--- resources/js/views/pageListGoodsOnStorages.vue
+++ resources/js/views/pageListGoodsOnStorages.vue
@@ -10,8 +10,9 @@
                 <div class="tab-controls tabs-small tabs-rounded" data-highlight="bg-green-dark">
                     <a href="#" class="no-effect bg-green-dark no-click" data-active="" data-bs-toggle="collapse" data-bs-target="#tab-8" aria-expanded="true">Товар</a>
                     <a href="#" class="no-effect collapsed" data-bs-toggle="collapse" data-bs-target="#tab-9" aria-expanded="false">Склад</a>
-                    <a href="#" class="no-effect collapsed" data-bs-toggle="collapse" data-bs-target="#tab-10" aria-expanded="false">движение</a>
+                    <a href="#" class="no-effect collapsed" data-bs-toggle="collapse" data-bs-target="#tab-10" aria-expanded="false">Движение</a>
                 </div>
+
                 <div class="clearfix mb-3"></div>
                 <div data-bs-parent="#tab-group-3" class="collapse show" id="tab-8" style="">
                     <span>Посмотреть остатки продукта на всех складах</span>
@@ -37,7 +38,15 @@
                 </div>
                 <div data-bs-parent="#tab-group-3" class="collapse" id="tab-9" style="">
                     <p class="mb-2 text-center">Посмотреть все продукты на выбранном складе</p>
-                    <select-input :data="list_storages" :label="'Склады'" :value="selected_storage" :keyOfValue="'id'" @getSelected="selectStorage"></select-input>
+
+                    <select-input
+                        :data="list_storages"
+                        :label="'Склады'"
+                        :value="selected_storage"
+                        :keyOfValue="'id'"
+                        @getSelected="selectStorage"
+                    ></select-input>
+
                     <div class="card card-style p-4 pt-3 mt-1" style="margin: auto 4px 30px 4px;">
                         <p class="text-center mb-0" v-if="selected_storage === 'default'">Нужно выбрать склад</p>
                         <p class="text-center mb-0" v-if="empty_storage">На выбраном складе нет товара</p>
@@ -52,7 +61,67 @@
                         </div>
                     </div>
                 </div>
-                <div data-bs-parent="#tab-group-3" class="collapse" id="tab-10" style="">dfgdf</div>
+                <div data-bs-parent="#tab-group-3" class="collapse" id="tab-10" style="">
+                    <span>Посмотреть движение продукта на всех складах</span>
+                    <v-select :options="available_goods"
+                              :value="'id'"
+                              :label="'name'"
+                              :placeholder="'выбрать продукт'"
+                              :map-keydown="handlers"
+                              @option:selected="showMovementsGoods"
+                              @search="searchGoods"
+                    >
+                    </v-select>
+
+                    <div class="content" id="tab-group-20">
+                        <div class="tab-controls tabs-small tabs-rounded" data-highlight="bg-green-dark">
+                            <a href="#" class="no-effect bg-green-dark no-click" data-active="" data-bs-toggle="collapse" data-bs-target="#tab-20" aria-expanded="true"><i class="fa fa-heart"></i></a>
+                            <a href="#" class="no-effect collapsed" data-bs-toggle="collapse" data-bs-target="#tab-21" aria-expanded="false"><i class="fa fa-star"></i></a>
+                        </div>
+                        <div class="clearfix mb-3"></div>
+
+                        <div data-bs-parent="#tab-group-20" class="collapse show" id="tab-20" style="">
+                            <div class="card card-style p-0 pt-3 mt-3" style="margin: 0 0 30px 0;">
+                                <p class="text-center" v-if="selected_goods === 'default'">Выберете продукт</p>
+                                <div v-if="loading_goods" class="spinner-border text-light" role="status" style="margin: 0 auto;">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+
+                                <div class="row m-0" v-for="(movement, index) in movements_goods" :key="movement.storage_id">
+                                    <div class="col-8">{{movement.storage_name_from}} ➠<sup>{{movement.category}}</sup>➠ {{movement.storage_name_to}} <br> <span class="opacity-40 font-10">{{movement.date_created}} {{movement.user_name_created}}</span></div>
+                                    <div class="col-2 align-content-end">
+                                        <span class="font-600">{{movement.amount}} <sup class="opacity-50">{{movement.goods_unit}}</sup></span> <br>
+                                        <span class="font-10">{{movement.price}}   <sup class="opacity-50">₴</sup> </span>
+                                    </div>
+                                    <div class="col-2 align-content-end ">
+                                        <div class="icon icon-m rounded-s shadow-l me-1 bg-twitter">
+                                            <i class="fa-solid fa-right-from-bracket font-25 bg-blue-dark rounded-s" v-if="movement.category==='move'"></i>
+                                            <i class="fas fa-check-to-slot font-25 bg-green-dark  rounded-s" v-if="movement.category==='ready'"></i>
+                                            <i class="fa-solid fa-recycle" v-if="movement.category==='correct'"></i>
+                                            <i class="fa-solid fa-seedling font-25 bg-green-dark rounded-s" v-if="movement.category==='grow'"></i>
+                                            <i class="fa-solid fa-dollar font-25 bg-green-dark rounded-s" v-if="movement.category==='buy'"></i>
+                                            <i class="fas fa-boxes-alt font-25 bg-gray-dark rounded-s" v-if="movement.category==='ingredients'"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div data-bs-parent="#tab-group-20" class="collapse" id="tab-21" style="">
+                            <div v-if="this.selected_goods" class="mt-3">
+                                <list-movements
+                                    :goods_id="this.selected_goods"
+                                >
+                                </list-movements>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+
+                </div>
             </div>
 
 
@@ -77,6 +146,8 @@
     import vSelect from "vue-select"
     import 'vue-select/dist/vue-select.css';
     import SelectInput from "../Components/SelectInput";
+    import listMovements from "../Components/listMovements";
+
 
 
     export default {
@@ -85,7 +156,8 @@
             error,
             TitlePage,
             headBar, NavBar, NavBarMenu,
-            SelectInput, vSelect
+            SelectInput, vSelect,
+            listMovements
         },
         data(){
             return {
@@ -98,7 +170,8 @@
                 selected_storage: 'default',
                 storage_goods: [],
                 loading_list_goods: false,
-                empty_storage: false
+                empty_storage: false,
+                movements_goods: []
             }
         },
         computed: {},
@@ -117,7 +190,7 @@
             getListStorages(){
               axios.get('/api/getListStorages/').then(res => {
                   this.list_storages = res.data.data;
-                  console.log(res.data)
+                  //console.log(res.data)
               }).catch(e => {
                   console.log(e)
               });
@@ -126,32 +199,21 @@
                 this.loading_list_goods = true;
                 this.storage_goods = [];
                 this.empty_storage = false;
-              axios.get(`/api/getStorageGoods/available/${this.selected_storage}/all`).then(res => {
-                  this.storage_goods = res.data.data.filter(el => Number.parseFloat(el.amount) !== 0 );
-                  if(this.storage_goods.length === 0){
-                      this.empty_storage = true;
-                  }
-                  this.loading_list_goods = false;
-              }).catch(e => {
-                  console.log(e)
-              });
+                axios.get(`/api/getStorageGoods/available/${this.selected_storage}/all`).then(res => {
+                    this.storage_goods = res.data.data.filter(el => Number.parseFloat(el.amount) !== 0 );
+                    if(this.storage_goods.length === 0){
+                        this.empty_storage = true;
+                    }
+                    this.loading_list_goods = false;
+                }).catch(e => {
+                    console.log(e)
+                });
             },
             selectStorage(value){
                 this.selected_storage = value;
                 if(value === 'default') return;
                 this.getStorageGoods();
             },
-            // async getStorageGoods(){
-            //     this.loading_goods = true;
-            //     const res = await axios.get(`/api/getListGoods`);
-            //     this.loading_goods = false;
-            //     if(!res.data){
-            //         console.error('Error get search')
-            //         return ;
-            //     }
-            //     this.available_goods = res.data.data;
-            //     console.log(res.data)
-            // },
             changeGoods(value){
                 this.selected_goods = value.id;
                 console.log('selected goods: '+this.selected_goods)
@@ -165,6 +227,51 @@
                     console.error(this.message)
                 })
 
+            },
+            showMovementsGoods(value){
+                this.selected_goods = value.id;
+                console.log('selected goods: '+this.selected_goods)
+                this.loading_goods = true;
+
+                //получить движение продукта по складам
+                axios.get('/api/getListGoodsMovementsOnStorages/'+this.selected_goods+'/2022-08-01/2022-10-01').then(res => {
+/*
+{
+  "id": 849,
+  "user_id_created": 31,
+  "user_name_created": "Наталья Коваль",
+  "date_created": "2022-08-27 19:17:17",
+  "storage_id_from": null,
+  "storage_name_from": null,
+  "storage_id_to": 3,
+  "storage_name_to": "Теплица-1",
+  "goods_id": 1,
+  "goods_name": "помидор",
+  "goods_unit": "кг",
+  "amount": "24.5",
+  "user_id_accepted": 31,
+  "user_name_accepted": "Наталья Коваль",
+  "price": null,
+  "order_main": null,
+  "date_accepted": "2022-08-27 19:17:17",
+  "link_id": null,
+  "category": "move"
+}
+*/
+                    console.log(res.data.data);
+                    this.movements_goods = res.data.data
+                }).catch(e => {
+                    this.message = e.response.data.message
+                    console.log(e)
+                });
+
+                // this.movements_goods = [
+                //     {"storage_name_from": "fff","amount":4444,"unit":"кг"},
+                //     {"storage_name_from": "fff","amount":4444,"unit":"кг"}
+                //     ]
+
+                // console.log(this.movements_goods)
+                this.loading_goods = false;
             },
             handlers: (map, vm) => ({
                 ...map,
